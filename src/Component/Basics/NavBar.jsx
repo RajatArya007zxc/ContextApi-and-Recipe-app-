5147a2413e79694250de742787bd0dc914b1a6d7
Rajat <rajatarya199zx@gmail.com>
2020-07-11 20:52:05 +0530

    consuming multiple context using Hooks

--- src/Component/Basics/NavBar.jsx
+++ src/Component/Basics/NavBar.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { ThemeContext } from '../../ContextComponent/BasicContext/ContextApi';
 import { AuthCon } from '../../ContextComponent/BasicContext/AuthContext';
-
+/*
 class NavBar extends Component {
    // static contextType=ThemeContext;
     
@@ -45,5 +45,23 @@ class NavBar extends Component {
         );
     }
 }
-
+*/
+const Navbar = () => {
+    const { isLightTheme, light, dark } = useContext(ThemeContext);
+    const { isAuth, verify } = useContext(AuthCon);
+    const theme = isLightTheme ? light : dark;
+    return (
+      <nav style={{ background: theme.ui, color: theme.syntax }}>
+        <h1>Context App</h1>
+        <div onClick={() => verify()}>
+          { isAuth ? 'Logged in' : 'Logged out' }
+        </div>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    );
+  }
 export default NavBar;
