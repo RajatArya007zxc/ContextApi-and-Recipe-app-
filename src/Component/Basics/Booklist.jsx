c729fe246526057029371f17034dcb709de23b6d
Rajat <rajatarya199zx@gmail.com>
2020-07-11 18:54:54 +0530

    useContext(hook) for consuming context in functional component

--- src/Component/Basics/Booklist.jsx
+++ src/Component/Basics/Booklist.jsx
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component,useContext } from 'react';
 import { ThemeContext } from '../../ContextComponent/BasicContext/ContextApi';
-
+/*
 class Booklist extends Component {
     static contextType=ThemeContext;
     render() {
@@ -19,5 +19,18 @@ class Booklist extends Component {
         );
     }
 }
-
+*/
+const BookList = () => {
+    const { isLightTheme, light, dark } = useContext(ThemeContext);
+    const theme = isLightTheme ? light : dark;
+    return ( 
+      <div className="book-list" style={{ color: theme.syntax, background: theme.bg }}>
+        <ul>
+          <li style={{ background: theme.ui }}>the way of kings</li>
+          <li style={{ background: theme.ui }}>the name of the wind</li>
+          <li style={{ background: theme.ui }}>the final empire</li>
+        </ul>
+      </div>
+    );
+  }
 export default Booklist;
