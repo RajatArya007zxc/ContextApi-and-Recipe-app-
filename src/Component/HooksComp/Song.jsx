9d59fa7c3b6f3c331779c4930c481b675804776c
Rajat <rajatarya199zx@gmail.com>
2020-07-11 18:02:29 +0530

    Add uuid package and multiple useState

--- src/Component/HooksComp/Song.jsx
+++ src/Component/HooksComp/Song.jsx
@@ -1,15 +1,19 @@
 import React,{useState} from 'react';
-//import uuid from' react-uuid'
+import uuid from 'react-uuid'
 
 
 const SongList = () => {
-    const [song,setFirst]=useState([{title:"Barbie Girl ",id:1},
-               {title:"Ginne merea",id:2},
-                    {title:"baby",id:3}])
+    const [song,setFirst]=useState([{title:"Barbie Girl ",id:1}, {title:"Ginne merea",id:2},{title:"baby",id:3}])
+    const [changeState,setChangeState]=useState('');
 
-              const  AddSongs=()=>{setFirst([...song,{title:"Gora Gora Rang tera",id:4 }])}
-                
-                
+    const  AddSongs=(w)=>{setFirst([...song,{title:w,id:uuid()}])}
+             
+         const handleSubmit=(r)=>{
+     
+             r.preventDefault();
+             AddSongs(changeState);
+             setChangeState('');
+         }       
 
 
     return (
@@ -24,7 +28,12 @@ const SongList = () => {
                )
            })}
            </ul>
-           <button onClick={AddSongs}>To Add</button>
+           
+           <form onSubmit={handleSubmit}>
+               <input type="text" value={changeState} onChange={(e)=>setChangeState(e.target.value)} required/>
+              <input type="submit" value="hey"/>
+           </form>
+           
         </div>
     );
 }
