fb4b108510a6b11f60587148988ffb09cd7b8e18
Rajat <rajatarya199zx@gmail.com>
2020-07-11 18:40:41 +0530

    How the UseEffect is Use when Component Render

--- src/Component/HooksComp/Song.jsx
+++ src/Component/HooksComp/Song.jsx
@@ -1,11 +1,11 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import uuid from 'react-uuid'
 
 
 const SongList = () => {
     const [song,setFirst]=useState([{title:"Barbie Girl ",id:1}, {title:"Ginne merea",id:2},{title:"baby",id:3}])
     const [changeState,setChangeState]=useState('');
-
+  const [age,setAge]=useState(23);
     const  AddSongs=(w)=>{setFirst([...song,{title:w,id:uuid()}])}
              
          const handleSubmit=(r)=>{
@@ -13,8 +13,16 @@ const SongList = () => {
              r.preventDefault();
              AddSongs(changeState);
              setChangeState('');
-         }       
+         } 
+         
+         useEffect(() => {
+            console.log('songs ',song)
 
+         }, [song]);
+
+         useEffect(() => {
+            console.log("age increment",age);
+         }, [age]);
 
     return (
 
@@ -33,7 +41,7 @@ const SongList = () => {
                <input type="text" value={changeState} onChange={(e)=>setChangeState(e.target.value)} required/>
               <input type="submit" value="hey"/>
            </form>
-           
+           <button onClick={()=>setAge(age+1)}>Add 1</button>
         </div>
     );
 }
